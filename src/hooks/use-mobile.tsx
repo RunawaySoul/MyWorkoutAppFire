918adfffcd8f670a8af7f6cbce82f2c78d050aa1
loga4 <lagachart5002@gmail.com>
2025-07-06 11:46:42 +0000

    Ошибка Error: Hydration failed because the server rendered HTML didn't m

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,17 +3,24 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+
+    // Set state on mount
+    setIsMobile(mql.matches)
+
+    const handleResize = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    mql.addEventListener("change", handleResize)
+
+    return () => {
+      mql.removeEventListener("change", handleResize)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
